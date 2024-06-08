f8127ed7cd40561911437a51bfc7b564aa53812a
Gaganpreet Singh <dmgaganpreet@gmail.com>
2024-06-08 20:29:49 +0530

    Moved errHandler middleware to bottom.

--- src/app.js
+++ src/app.js
@@ -44,11 +44,12 @@ app.use(express.json());
 app.use("/api", routes);
 
 // Middleware to handle Errors.
-app.use(errorHandler);
 
 // send back a 404 error for any unknown api request
 app.use((req, res, next) => {
   next(new ApiError(404, "Not found"));
 });
+
+app.use(errorHandler);
 
 module.exports = app;
