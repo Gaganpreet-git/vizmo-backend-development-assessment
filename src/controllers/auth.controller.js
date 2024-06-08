4d2820cf0c4b717202402977a057afd596783bab
Gaganpreet Singh <dmgaganpreet@gmail.com>
2024-06-08 14:19:37 +0530

    Added documentation comments in controllers.

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,11 +1,29 @@
 const { authService, userService, tokenService } = require("../services");
 const catchAsync = require("../utils/CatchAsync");
 
+/**
+ * Registers a new user with the provided user data.
+ *
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @returns {Promise<void>} - A promise that resolves when the registration is successful.
+ */
+
 const register = async (req, res) => {
   const user = await userService.createUser(req.body);
   const tokens = await tokenService.generateAuthTokens(user);
   res.status(201).send({ user, tokens });
 };
+
+/**
+ * Logs in a user with the provided email and password.
+ *
+ * @param {Object} req - The request object containing the user's email and password.
+ * @param {string} req.body.email - The email address of the user.
+ * @param {string} req.body.password - The password of the user.
+ * @param {Object} res - The response object.
+ * @returns {Promise<void>} - A promise that resolves when the login is successful.
+ */
 
 const login = async (req, res) => {
   const { email, password } = req.body;
--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -1,8 +1,18 @@
 const { postService } = require("../services");
 const catchAsync = require("../utils/CatchAsync");
 
-// Function to add new post
-const addNewPost = async (req, res) => {
+/**
+ * Adds a new post with the provided data.
+ *
+ * @param {Object} req - The request object.
+ * @param {Object} req.body - The data for the new post.
+ * @param {string} req.body.title - The title of the post.
+ * @param {string} req.body.content - The content of the post.
+ * @param {Object} req.user - The user object extracted from the JWT token.
+ * @param {string} req.user.sub - The ID of the authenticated user.
+ * @param {Object} res - The response object.
+ * @returns {Promise<void>} - A promise that resolves when the post is successfully added.
+ */ const addNewPost = async (req, res) => {
   const author = req.user.sub;
   console.log(req.user);
   const post = await postService.createPost({ ...req.body, author });
@@ -17,14 +27,43 @@ const getAllPosts = async (req, res) => {
   res.status(200).json(post);
 };
 
-// Function to get details of a single post
+/**
+ * Retrieves all posts with optional filtering by title and/or author.
+ *
+ * @param {Object} req - The request object.
+ * @param {Object} req.query - The query parameters for filtering.
+ * @param {string} req.query.title - The title to filter posts by.
+ * @param {string} req.query.author - The author to filter posts by.
+ * @param {Object} res - The response object.
+ * @returns {Promise<void>} - A promise that resolves with the filtered posts.
+ */
+
+/**
+ * Retrieves details of a single post by its ID.
+ *
+ * @param {Object} req - The request object.
+ * @param {string} req.params.postId - The ID of the post to retrieve.
+ * @param {Object} res - The response object.
+ * @returns {Promise<void>} - A promise that resolves with the details of the post.
+ */
+
 const getPost = async (req, res) => {
   const { postId } = req.params;
   const post = await postService.getPostById(postId);
   res.status(200).json(post);
 };
 
-// Function to delete a post
+/**
+ * Deletes a post by its ID if the authenticated user is its author.
+ *
+ * @param {Object} req - The request object.
+ * @param {string} req.params.postId - The ID of the post to delete.
+ * @param {Object} req.user - The user object extracted from the JWT token.
+ * @param {string} req.user.sub - The ID of the authenticated user.
+ * @param {Object} res - The response object.
+ * @returns {Promise<void>} - A promise that resolves when the post is successfully deleted.
+ */
+
 const deletePost = async (req, res) => {
   const { postId } = req.params;
   const author = req.user.sub;
@@ -32,7 +71,18 @@ const deletePost = async (req, res) => {
   res.status(204).json(post);
 };
 
-// Function to update a post
+/**
+ * Updates a post by its ID if the authenticated user is its author.
+ *
+ * @param {Object} req - The request object.
+ * @param {string} req.params.postId - The ID of the post to update.
+ * @param {Object} req.body - The updated data for the post.
+ * @param {Object} req.user - The user object extracted from the JWT token.
+ * @param {string} req.user.sub - The ID of the authenticated user.
+ * @param {Object} res - The response object.
+ * @returns {Promise<void>} - A promise that resolves with the updated post.
+ */
+
 const updatePost = async (req, res) => {
   const { postId } = req.params;
   const postData = req.body;
