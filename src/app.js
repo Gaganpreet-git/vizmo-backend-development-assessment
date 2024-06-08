f43df362eeec06100ec0d60bbad1b2689ade81ac
Gaganpreet Singh <dmgaganpreet@gmail.com>
2024-06-08 10:58:13 +0530

    Rerouted all api requesta to from /api to routes. Added 404 for invalid routes.

--- src/app.js
+++ src/app.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const errorHandler = require("./middlewares/errorHandler");
 const app = express();
+const routes = require("./routes/");
 
 app.use(express.json());
 
+// Reroute all api requests to routes
+app.use("/api", routes);
+
 // Middleware to handle Errors.
 app.use(errorHandler);
+
+// send back a 404 error for any unknown api request
+app.use((req, res, next) => {
+  next(new ApiError(404, "Not found"));
+});
 
 module.exports = app;
