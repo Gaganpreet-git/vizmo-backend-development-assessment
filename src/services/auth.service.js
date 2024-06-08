ec1536f567310ae875e8ad6deb4992ed4c45b8ca
Gaganpreet Singh <dmgaganpreet@gmail.com>
2024-06-08 10:26:31 +0530

    Added auth service to faciliate login with email and password.

---
+++ src/services/auth.service.js
@@ -0,0 +1,14 @@
+const userService = require("./user.service");
+const ApiError = require("../utils/ApiError");
+
+const loginUserWithEmailAndPassword = async (email, password) => {
+  const user = await userService.getUserByEmail(email);
+  if (!user || !(await user.isPasswordMatch(password))) {
+    throw new ApiError(401, "Incorrect Credentials");
+  }
+  return user;
+};
+
+module.exports = {
+  loginUserWithEmailAndPassword,
+};
