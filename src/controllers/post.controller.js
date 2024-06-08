d87261a454bf66ab67d38eb124f0565fbd6bf9cc
Gaganpreet Singh <dmgaganpreet@gmail.com>
2024-06-08 12:02:34 +0530

    Fixed typo in name

---
+++ src/controllers/post.controller.js
@@ -0,0 +1,28 @@
+const { postService } = require("../services");
+const catchAsync = require("../utils/CatchAsync");
+
+// Function to add new post
+const addNewPost = async (req, res) => {
+  const post = await postService.createPost(req.body);
+  res.status(201).json(post);
+};
+
+// Function to get all posts with filtering
+const getAllPosts = async (req, res) => {};
+
+// Function to get details of a single post
+const getPost = async (req, res) => {};
+
+// Function to delete a post
+const deletePost = async (req, res) => {};
+
+// Function to update a post
+const updatePost = async (req, res) => {};
+
+module.exports = {
+  addNewPost: catchAsync(addNewPost),
+  getAllPosts: catchAsync(getAllPosts),
+  getPost: catchAsync(getPost),
+  deletePost: catchAsync(deletePost),
+  updatePost: catchAsync(updatePost),
+};
