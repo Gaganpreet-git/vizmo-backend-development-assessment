771062e9f40b35b02dc70ab52fe6bbebc6d999fe
Gaganpreet Singh <dmgaganpreet@gmail.com>
2024-06-08 11:48:25 +0530

    Added all /posts routes along with thier controller function.

---
+++ src/routes/post.routes.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const { postController } = require("../controllers");
+const router = express.Router();
+
+// Route to add new post
+router.post("/", postController.addNewPost);
+
+// Route to get all posts also support filter by title & author
+router.get("/", postController.getAllPosts);
+
+// Route to get details of single blog post
+router.get("/:postId", postController.getPost);
+
+// Route to delete blog post
+router.delete("/:postId", postController.deletePost);
+
+// Route to update blog post
+router.put("/:postId", postController.updatePost);
