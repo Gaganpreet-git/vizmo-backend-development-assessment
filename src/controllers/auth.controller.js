4554d995f6a8070e5886055aaa736590b8f42141
Gaganpreet Singh <dmgaganpreet@gmail.com>
2024-06-08 10:30:06 +0530

    Added auth contoller with register and login fns.

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,20 @@
+const { userService, tokenService } = require("../services");
+const catchAsync = require("../utils/CatchAsync");
+
+const register = async (req, res) => {
+  const user = await userService.createUser(req.body);
+  const tokens = await tokenService.generateAuthTokens(user);
+  res.status(201).send({ user, tokens });
+};
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await authService.loginUserWithEmailAndPassword(email, password);
+  const tokens = await tokenService.generateAuthTokens(user);
+  res.status(200).json({ user, tokens });
+};
+
+module.exports = {
+  register: catchAsync(register, login),
+  login: catchAsync(login),
+};
